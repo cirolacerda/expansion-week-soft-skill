07f4cd812c9f154cdefa80aa7bdc421ec4dce84f
Ciro Botelho Lacerda <cirolacerda@gmail.com>
2020-11-18 23:55:37 -0300

    Adicionando styles global

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
 import React from 'react';
+import GlobalStyle from './styles/global';
 
-const App: React.FC = () => <h1>Soft Skill</h1>;
-
+const App: React.FC = () => {
+  return (
+    <>
+      <h1>Soft Skill</h1>
+      <h1>A Soft Skill que os desenvolvedores devem ter! </h1>
+      <GlobalStyle />
+    </>
+  );
+};
 export default App;
