40ca8fff4881c373708c3ff8e4d270d5f227de0c
Ciro Botelho Lacerda <cirolacerda@gmail.com>
2020-11-21 00:24:48 -0300

    Melhorias no layout principal

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,9 +1,7 @@
 import React from 'react';
-import {
-  HeaderContent, LogoContent,
-} from './styles';
+import { HeaderContent, InforBar, LogoContent } from './styles';
 
-import LogoMega from '../../assets/megafone.png';
+import LogoMentor from '../../assets/mentorslogo.png';
 
 import Logo from '../../assets/network.svg';
 
@@ -11,18 +9,38 @@ const Header: React.FC = () => (
   <>
     <HeaderContent>
       <header>
-        <img src={LogoMega} alt="Open Mentor" />
-        <p>Open Mentor</p>
+        <img src={LogoMentor} alt="Open Mentor" />
+        <strong>Open Mentor</strong>
       </header>
     </HeaderContent>
     <LogoContent>
       <div className="title">
         <h1>Comunicação é o diferencial</h1>
-        <p>A Soft Skill que todos os desenvolvedores devem ter! </p>
+        <p>
+          A Soft Skill que todos os desenvolvedores precisam ter para alcançar
+          as melhores oportunidades!
+        </p>
         <button type="button">Quero dominar a comunicação</button>
       </div>
       <img src={Logo} alt="Comunicação" />
     </LogoContent>
+
+    <InforBar>
+      <ul>
+        <li>
+          <strong>+100K</strong>
+          <span>Desenvolvedores</span>
+        </li>
+        <li>
+          <strong>+10K</strong>
+          <span>Países</span>
+        </li>
+        <li>
+          <strong>+30K</strong>
+          <span>Empresas</span>
+        </li>
+      </ul>
+    </InforBar>
   </>
 );
 
--- src/components/Header/styles.ts
+++ src/components/Header/styles.ts
@@ -1,76 +1,94 @@
 import styled from 'styled-components';
 
+export const InforBar = styled.div`
+  height: 85px;
+  margin: 0 150px;
+  background-color: #f8f8f8;
+  border-radius: 10px;
+
+  ul {
+    display: flex;
+    list-style: none;
+    justify-content: space-between;
+  }
+
+  li {
+    height: 85px;
+    width: 100%;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    padding: 0px 15px;
+
+    strong {
+      color: black;
+    }
+
+    span {
+      color: black;
+    }
+  }
+`;
+
 export const LogoContent = styled.div`
- height: 500px;
- margin: 0 80px;
- display: flex;
-
-
-.title {
-
+  height: 480px;
+  margin: 0 80px;
   display: flex;
-  flex: 1;
-  align-items: flex-start;
-  justify-content:center;
-  flex-direction: column;
 
-  h1 {
-    font-size: 3.5em;
-    color: #86C171;
+  .title {
+    display: flex;
+    flex: 1;
+    align-items: flex-start;
+    justify-content: center;
+    flex-direction: column;
 
+    h1 {
+      font-size: 3.5em;
+      color: #86c171;
+    }
+
+    p {
+      justify-content: left;
+      margin: 35px 0px;
+    }
+
+    button {
+      border-radius: 8px;
+      border: 1;
+      border-color: transparent;
+      background: #86c171;
+      color: #fff;
+      display: flex;
+      flex-direction: row;
+      align-items: center;
+      padding: 30px;
+      font-size: 1.5em;
+      border-bottom: 9px solid black;
+    }
   }
 
-  p {
-    justify-content: left;
-    margin: 35px 0px;
+  img {
+    width: 50%;
+    display: flex;
+    align-content: center;
   }
-
-  button {
-
-          border-radius: 8px;
-          border: 1;
-          border-color: transparent;
-          background: #86C171;
-          color: #fff;
-          display: flex;
-          flex-direction: row;
-          align-items: center;
-          padding: 30px;
-          font-size: 1.5em;
-          border-bottom: 9px solid black;
-
-
-        }
-
-
-
-}
-
-img {
-  width: 50%;
-  display: flex;
-  align-content: center;
-}
-
 `;
 
 export const HeaderContent = styled.div`
   display: flex;
   flex-direction: row;
+
   img {
-  width: 30%;
-  display: flex;
-  align-content: center;
-  margin: 10px;
+    width: 17%;
+    display: flex;
+    align-content: center;
+    margin: 5px 25px 0px;
   }
 
-  p {
-    display: flex;
+  strong {
     color: #ffffff;
-    text-align: start;
-  }
-  h3 {
-    margin-top: 0px;
+    margin-left: 17px;
   }
 `;
 
